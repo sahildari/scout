d2c01d8a32cb78eb637dad077e8ca38900840b02
Sahil Dari <52995565+sahildari@users.noreply.github.com>
2023-01-31 00:50:39 +0530

    Update scout.sh

--- scout.sh
+++ scout.sh
@@ -4,15 +4,28 @@ CYAN="\e[36m"
 ENDCOLOR="\e[0m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
+BLUE="\e[34m"
 
 ## Introduction
 
 mkdir -p output
-echo -ne "${GREEN} [+] Hello fellow hackers ${ENDCOLOR}\n"
+echo -ne ''' 
+ _____ _____ _____ _   _ _____ 
+/  ___/  __ \  _  | | | |_   _|
+\ `--.| /  \/ | | | | | | | |  
+ `--. \ |   | | | | | | | | |  
+/\__/ / \__/\ \_/ / |_| | | |  
+\____/ \____/\___/ \___/  \_/  
 
-echo -ne "${YELLOW} [+] Specify the programming language ${ENDCOLOR}
+Made with bash by Sahil <3
+
+'''
+
+echo -ne "${BLUE}[+] Hello fellow hackers ${ENDCOLOR}\n"
+echo -ne "${BLUE}[+] Specify the programming language
 1. ASP.Net
 2. SpringBoot
+3. Quit
 
 (Enter 1 for ASP.Net and 2 for SpringBoot)\n"
 
@@ -39,14 +52,32 @@ then
 	find . -name \*.cs -type f | while read line; do echo "$line" | tee -a csFile.txt;done
 	sort -u csFile.txt >> SortedCSFile.txt
 	cat SortedCSFile.txt| while read filename; do echo $filename;cat $filename |grep -in 'x-xss-protection\|x-frame-options\|x-content\|strict-transport-policy\|content-security-policy\|referrer-policy\|x-aspnet-version\|x-powered-by\|hsts\|cookie.httponly\|cookie.secure\|cookie.samesite\|logging\.\|corsbuilder';echo done;done | tee -a output/CSOutput.txt
+elif [ $language -eq 2 ]
+then
+	echo -ne "${YELLOW} Still in progress......"
+else
+	exit $1
+	# output
 	clear
-	echo -ne "${GREEN} [+] The sensitive data in the JSON files have been written to JSONoutput.txt ${ENDCOLOR}\n"
-	echo -ne "${YELLOW} [+] The sensitive data in the config files have been written to configOutput.txt ${ENDCOLOR}\n"
-	echo -ne "${CYAN} [+] Headers present in the config files:\n";cat output/configOutput.txt | grep -i 'x-xss-protection\|x-frame-options\|x-content\|strict-transport-policy\|content-security-policy\|referrer-policy\|x-aspnet-version\|x-powered-by\|server'
-	echo -ne "${GREEN} [+] The Logging level is as follows:\n"
-	cat output/CSOutput.txt | grep logging | grep -iv '\\'
+	echo ''' 
+	 _____ _____ _____ _   _ _____ 
+	/  ___/  __ \  _  | | | |_   _|
+	\ `--.| /  \/ | | | | | | | |  
+	 `--. \ |   | | | | | | | | |  
+	/\__/ / \__/\ \_/ / |_| | | |  
+	\____/ \____/\___/ \___/  \_/  
+
+	Made with bash by Sahil <3
+
+	'''
+	echo -ne "${YELLOW} [+] The sensitive data in the JSON files have been written to JSONoutput.txt ${ENDCOLOR}\n"
+	echo -ne "${CYAN} [+] The sensitive data in the config files have been written to configOutput.txt ${ENDCOLOR}\n"
+	echo -ne "${YELLOW} [+] Headers present in the config files:\n";cat output/configOutput.txt | grep -i 'x-xss-protection\|x-frame-options\|x-content\|strict-transport-policy\|content-security-policy\|referrer-policy\|x-aspnet-version\|x-powered-by\|server'
+	echo -ne "${CYAN} [+] The Logging level is as follows:\n";cat output/CSOutput.txt | grep logging | grep -iv '//'
 	# springboot
 	#.java
 	#.xml
 	#.java
 fi
+
+ 
